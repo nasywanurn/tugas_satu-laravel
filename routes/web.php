6d3100229ab57bf39542c05fd1018710d3e334f3
nasywa554 <nasywanurnafilah554@gmail.com>
2021-10-07 11:12:05 +0700

    tugas kesatu

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    return view('layouts.index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('layouts.about');
+})->name('about');
+
+Route::get('/post', function () {
+    return view('layouts.post');
+})->name('post');
+
+Route::get('/contact', function () {
+    return view('layouts.contact');
+})->name('contact');
